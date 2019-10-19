8008b37b298c856b5c63d853ba3386b2b713f6a6
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2019-10-18 23:32:13 -0300

    Initial tutorial

---
+++ DiscordBot/Bot.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class Bot
+    {
+        private const String token = "";
+
+        public static void Main(string[] args)
+            => new Bot().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
